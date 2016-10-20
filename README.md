5e7d8ac570d3f89bcdab802498b81e289cd4398a
Martin Buck <buckmartin@buckmartin.de>
2016-10-20 22:56:11 +0200

    minor changes to move_append(), added LICENSE, TODO and README

---
+++ README.md
@@ -0,0 +1,13 @@
+LazyTrees
+==========
+A C++ library for lazy tree structures.
+`LazyTrees` offers lazy search trees (e.g. KD-Tree). Usually such trees are fully-built and can then be used to search nearest neighbours efficently. 
+The drawback is that the tree has to be built, which can be rather time consuming. A lazy tree will only be built once needed and even then only its affected branches.
+
+Version
+-------
+0.1.0
+
+License
+------
+MIT (See LICENSE)
--- inc/LazyKdTree.h
+++ inc/LazyKdTree.h
@@ -1,4 +1,15 @@
-///@todo copyright
+/*
+    Copyright (c) 2016 Martin Buck
+    Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+    to deal in the Software without restriction, including without limitation the rights to
+    use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+    and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+    The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+    DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+    OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
 
 #ifndef LAZYKDTREE_H
 #define LAZYKDTREE_H
@@ -393,15 +404,16 @@ private:
 
     static inline void move_append(std::vector<P>&& from, std::vector<P>& to)
     {
-        if (to.empty())
+        if (to.empty()) {
             to = std::move(from);
-        else {
-            to.reserve(to.size() + from.size());
-            to.insert(
-                std::end(to),
-                std::make_move_iterator(std::begin(from)),
-                std::make_move_iterator(std::end(from)));
+            return;
         }
+
+        to.reserve(to.size() + from.size());
+        to.insert(
+            std::end(to),
+            std::make_move_iterator(std::begin(from)),
+            std::make_move_iterator(std::end(from)));
     }
 
     static inline void remove_from(std::vector<P>& x, size_t index)
